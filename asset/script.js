0af36aa095cd8122318232612a9c43947ba123df
MbeAlexPFS <alexmbe8921@gmail.com>
2025-03-14 11:20:23 +0000

    Ajout de popup, systeme de like, verificateur de commentaire, pseudo systeme de commande, systeme de recherche.

--- asset/script.js
+++ asset/script.js
@@ -79,3 +79,130 @@ if (document.getElementById("form1") != undefined) {
         alert("Erreur de connexion")
     }
 }
+
+//popup 2 funcs
+function closepopup(element) {
+    document.querySelector(element).style.display = "none"
+}
+
+function openpopup(element) {
+    document.querySelector(element).style.display = "flex"
+}
+
+//increment 2 func
+function increment(element,inc,once) {
+    if (document.querySelector(element) != undefined) {
+        document.querySelector(element).textContent = parseInt(document.querySelector(element).textContent) + inc
+    if (once[0]) {
+        document.querySelector(element).setAttribute("id", "")
+        document.querySelector(once[1]).classList.add("p-0")
+        document.querySelector(once[1]).classList.add("raduis-100")
+        document.querySelector(once[1]).classList.add("bg-blue")
+    }
+    }
+}
+
+//verification commentaire 2func
+function checkComment(toverify,ondanger,onsuccess) {
+    let success = true
+    let found = []
+    const grosMots = [
+        "putain", "merde", "con", "connard", "salope", "pute", "enculé", 
+        "bite", "couille", "cul", "foutre", "baise", "enfoiré", "bordel", 
+        "salaud", "niquer", "connasse", "bâtard", "chier", "branler"
+    ];
+    toverify.forEach(element => {
+        let el = document.querySelector(element)
+        grosMots.forEach( (gm) => {
+            if (el.value.includes(gm)) {
+                success = false
+                if (!found.includes(gm)) {
+                    found.push(gm)
+                } 
+            }
+        })
+    });
+    if (success) {
+        document.querySelector(onsuccess).style.display = "initial"
+        document.querySelector(ondanger).style.display = "none"
+    }else {
+        document.querySelector(onsuccess).style.display = "none"
+        document.querySelector(ondanger).style.display = "initial"
+        document.querySelector(ondanger).innerHTML = 'Commentaire inapproprié, gros mots trouvés: <br> <ul>'
+        for (const fgm of found) {
+            document.querySelector(ondanger).innerHTML += '<li>'+fgm+'</li>'  
+        }
+        document.querySelector(ondanger).innerHTML += '</ul>'
+    }
+}
+
+//pseudo commande 3func
+let data = []
+function command(lib, price, time) {
+    let commanded = false
+    data.forEach((dt) => {
+        if (dt[0] == lib) {
+            commanded = true    
+        }    
+    })
+
+    if (commanded) {
+        alert('Vous avez déja commandé cet article')    
+    }else{
+        alert("L'article a été ajouté au panier avec succès")
+        data.push([lib,price,time])
+        tableUpd()
+    }
+}
+
+let totprice = 0
+let tottime = 0
+function tableUpd () {
+    totprice = 0
+    tottime = 0
+    let table = document.querySelector('#table')
+    table.innerHTML = `<thead><th>Formation</th><th>Prix</th><th>Durée</th></thead><tbody>`
+    for (const td of data) {
+        table.innerHTML += `<tr><td>${td[0]}</td><td>${td[1]}fcfa</td><td>${td[2]}jours</td></tr>`
+        totprice += td[1]
+        tottime += td[2]
+    }
+    table.innerHTML += `</tbody><tfoot><td class="bg-orange">Total</td><td>${totprice}fcfa</td><td>${tottime}jours</td></tfoot>`
+}
+
+function buy() {
+    if (data.length == 0) {
+        alert("Il n'ya rien à payer")
+    }else {
+        data = []
+        alert("Achat effectué avec succès. Montant total: "+totprice+" fcfa")
+        tableUpd()
+    }
+}
+
+function cancel() {
+    if (data.length == 0) {
+        alert("Le panier est déja vide")
+    }else {
+        data = []
+        tableUpd()
+    }
+}
+
+//search 1 func
+let input = document.querySelector(".input")
+input.oninput = () => {
+    let titles = document.querySelectorAll(".title")
+    titles.forEach((title) => {
+        if (!title.textContent.toLowerCase().includes(input.value.toLowerCase())) {
+            title.parentNode.style.display = "none"
+            if (input.value == "") {
+                title.parentNode.style.display = "initial"
+            }
+        }else{
+            title.parentNode.style.display = "initial"
+        }
+    })
+}
+
+//19 functions
